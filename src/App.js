585f0064386a93c0009a40f2bc246110b6069be9
AnnaHort <annahort3d@gmail.com>
2024-09-26 22:33:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import Header from "./components/Header/Header.jsx";
-import { AppContainer } from "./App.styled";
-import { Route, Routes } from "react-router-dom";
+import React from 'react';
+import Header from './components/Header/Header.jsx';
+import { AppContainer } from './App.styled';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <AppContainer>
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route path="/healthy" element={<Header />} />
 
         {/* <Header /> */}
       </Routes>
